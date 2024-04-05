6a72a31cfb219749192db38256602bbe66c03264
BatyrhanB <batyrhanbolotbek@gmail.com>
2024-04-05 16:20:58 +0600

    changed gunicorn and docker port

--- scripts/wsgi_server.sh
+++ scripts/wsgi_server.sh
@@ -9,4 +9,4 @@ python manage.py migrate
 echo "Collect static"
 python manage.py collectstatic --no-input
 echo "Run server"
-gunicorn --workers=5 --threads=2 --worker-class=gthread -b 0.0.0.0:8000 config.wsgi --reload
+gunicorn --workers=5 --threads=2 --worker-class=gthread -b 0.0.0.0:8222 config.wsgi --reload
